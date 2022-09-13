4b951f0df6fbd8d4a92831fc819fc6b5b90330ef
shaitemp88 <105381212+shaitemp88@users.noreply.github.com>
2022-09-13 09:58:15 +0300

    remove excessive spaces

--- Code/run_app.py
+++ Code/run_app.py
@@ -12,7 +12,6 @@ REGION = os.environ['region']
 # AWS_KEY = os.environ.get('aws_secret_access_key')
 # REGION = os.environ.get('region')
 
-
 print("=                   =")
 print("Envoronment Variable:")
 print(AWS_ID)
@@ -20,12 +19,10 @@ print(AWS_KEY)
 print(REGION)
 print("=====================")
 
-
 ec2_resource = boto3.resource('ec2', region_name=REGION)
 instances = ec2_resource.instances.filter(
     Filters=[{'Name': 'tag:k8s.io/role/master', 'Values': [1]}, {'Name': 'instance-state-code', 'Values': [16]}])
 for instance in instances:
     print(instance.id, instance.instance_type)
-
 
 print("===============!!!FIINSHED!!!===============")
